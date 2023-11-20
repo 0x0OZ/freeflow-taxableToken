bb415df3b01e1174e5d1970cc689ce238a9e83d5
0xOZ <abuzaid.sahab.2015@gmail.com>
2023-11-20 16:07:34 +0000

    finishing tests

--- test/Lock.t.sol
+++ test/Lock.t.sol
@@ -49,14 +49,22 @@ contract LockScript is Test {
 
     function test_lockTokens(uint amount) public {
         amount = bound(amount, 1, 1e6);
+
+        assertEq(lock.areTokensLocked(user), false);
         lockTokens(user, amount);
-        lockTokens(anotherUser, amount);
         (uint256 lockedAmount, uint64 unlockTime) = lock.lockedBalances(user);
         assertEq(lockedAmount, amount);
         assertEq(unlockTime, uint64(block.timestamp + 1 days));
+        assertEq(lock.areTokensLocked(user), true);
+
+
+        assertEq(lock.areTokensLocked(anotherUser), false);
+        lockTokens(anotherUser, amount);
+
         (lockedAmount, unlockTime) = lock.lockedBalances(anotherUser);
         assertEq(lockedAmount, amount);
         assertEq(unlockTime, uint64(block.timestamp + 1 days));
+        assertEq(lock.areTokensLocked(anotherUser), true);
     }
 
     function test_lockZeroTokensRevert() public {
@@ -72,6 +80,7 @@ contract LockScript is Test {
         prepareLockTokens(user, lockAmount);
         vm.startPrank(user);
         lock.lockTokens(lockAmount, 1 days);
+        // prepareLockTokens(user, lockAmount);
         vm.expectRevert(Lock.TokensAlreadyLocked.selector);
         lock.lockTokens(lockAmount, 1 days);
         vm.stopPrank();
@@ -80,10 +89,13 @@ contract LockScript is Test {
     function test_reLockTokens(uint lockAmount) public {
         lockAmount = bound(lockAmount, 1, 1e6);
         prepareLockTokens(user, lockAmount);
+        assertEq(lock.areTokensLocked(user), false);
         vm.prank(user);
         lock.lockTokens(lockAmount, 1 days);
+        assertEq(lock.areTokensLocked(user), true);
         // unlock first
         skip(1 days);
+        assertEq(lock.areTokensLocked(user), false);
         //
         prepareLockTokens(user, lockAmount);
         vm.prank(user);
@@ -91,10 +103,10 @@ contract LockScript is Test {
         (uint256 lockedAmount, uint64 unlockTime) = lock.lockedBalances(user);
         assertEq(lockedAmount, lockAmount);
         assertEq(unlockTime, uint64(block.timestamp + 1 days));
+        assertEq(lock.areTokensLocked(user), true);
     }
 
-
-    function test_reLockTokensBeforeUnlockRevert() public{
+    function test_reLockTokensBeforeUnlockRevert() public {
         uint lockAmount = 1e2;
         console.log("time: ", block.timestamp);
         prepareLockTokens(user, lockAmount);
@@ -104,16 +116,15 @@ contract LockScript is Test {
         (uint lockedAmount, uint64 unlockTime) = lock.lockedBalances(user);
         assertEq(lockedAmount, lockAmount);
         assertEq(unlockTime, uint64(block.timestamp + 7 days));
-        
+
         prepareLockTokens(user, lockAmount);
-        skip(1 days);
         console.log("time: ", block.timestamp);
         vm.prank(user);
         vm.expectRevert(Lock.TokensStillLocked.selector);
         lock.reLockTokens(lockAmount, 7 days);
-
     }
-    function test_reLockTokensWithDifferentAmount(uint lockAmount)public{
+
+    function test_reLockTokensWithDifferentAmount(uint lockAmount) public {
         lockAmount = bound(lockAmount, 1, 1e3);
         prepareLockTokens(user, lockAmount);
         vm.prank(user);
@@ -121,13 +132,37 @@ contract LockScript is Test {
         // unlock first
         skip(1 days);
         //
-        prepareLockTokens(user, lockAmount + 1);
+        uint upperLockAmount = lockAmount ^ 2;
+        prepareLockTokens(user, upperLockAmount);
         vm.prank(user);
-        lock.reLockTokens(lockAmount + 1, 1 days);
+        lock.reLockTokens(upperLockAmount, 1 days);
         (uint256 lockedAmount, uint64 unlockTime) = lock.lockedBalances(user);
-        assertEq(lockedAmount, lockAmount + 1);
+        assertEq(lockedAmount, upperLockAmount);
         assertEq(unlockTime, uint64(block.timestamp + 1 days));
+
+        uint lowerLockAmount = lockAmount / 2;
+        if (lowerLockAmount == 0) lowerLockAmount = 1;
+
+        skip(1 days);
+        prepareLockTokens(user, lowerLockAmount);
+        vm.prank(user);
+        lock.reLockTokens(lowerLockAmount, 1 days);
     }
 
+    function test_withdrawTokens(uint lockAmount) public {
+        lockAmount = bound(lockAmount, 1, 1e3);
+        lockTokens(user, lockAmount);
+        (uint lockedAmount, uint64 unlockTime) = lock.lockedBalances(user);
+        assertEq(unlockTime, uint64(block.timestamp + 1 days));
+        assertEq(lockedAmount, lockAmount);
 
+        // unlock first
+        skip(1 days);
+        //
+        vm.prank(user);
+        lock.withdraw();
+        (lockedAmount, unlockTime) = lock.lockedBalances(user);
+        assertEq(lockedAmount, 0);
+        assertEq(unlockTime, 0);
+    }
 }
